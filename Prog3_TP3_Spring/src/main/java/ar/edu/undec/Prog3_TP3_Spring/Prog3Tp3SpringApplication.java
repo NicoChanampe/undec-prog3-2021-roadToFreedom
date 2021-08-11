3f206ba96f2a87b03a2340974e0f46ca0d88d614
NicoChanampe <champis_97@hotmail.com.ar>
2021-08-11 16:06:29 -0300

    TP3_Spring[Se creo proyecto]

---
+++ Prog3_TP3_Spring/src/main/java/ar/edu/undec/Prog3_TP3_Spring/Prog3Tp3SpringApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.Prog3_TP3_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3Tp3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3Tp3SpringApplication.class, args);
+	}
+
+}
---
+++ Prog3_TP3_Spring/src/test/java/ar/edu/undec/Prog3_TP3_Spring/Prog3Tp3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.Prog3_TP3_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3Tp3SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
