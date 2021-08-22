3fad1911c5b43d6edfa4e913dda2b8db15b33430
NicoChanampe <champis_97@hotmail.com.ar>
2021-08-21 22:07:57 -0300

    TP3_Spring[se genero test y codigo para que al setear una altura simule falla en BD]

--- Prog3_TP3_Spring/src/test/java/testCasoUso/TestCUModificarPersona.java
+++ Prog3_TP3_Spring/src/test/java/testCasoUso/TestCUModificarPersona.java
@@ -91,4 +91,20 @@ public class TestCUModificarPersona {
         assertEquals(1.8,nuevaPersona.getAltura());
         assertTrue(simulacro.modificarAlturaPersona("87630115",1.8));
     }
+
+    @Order(5)
+    @Test
+    public void test05_modificarPersona_alturaSeModifica_fallaBD()throws ExceptionPersona{
+        //Arrange
+        LocalDate fechaNacimiento = LocalDate.parse("1999-01-20");
+        Persona nuevaPersona = Persona.factoryPersona(1,"Franco David","Cardozo","Racing",fechaNacimiento,"87630115",1.69,59.7);
+        ModificarPersonaCU simulacro = new ModificarPersonaCU(iTraerPersona,iModificarPersona);
+        //Act
+        Mockito.when(iTraerPersona.damePersonaSegunDni("87630115")).thenReturn(nuevaPersona);
+        nuevaPersona.setAltura(1.8);
+        Mockito.when(iModificarPersona.modificarAltura(nuevaPersona)).thenReturn(false);
+        //Assert
+        assertEquals(1.8,nuevaPersona.getAltura());
+        assertFalse(simulacro.modificarAlturaPersona("87630115",1.8));
+    }
 }
