98c168aeae2091a846977ef3fd7e9622dbc91db8
NicoChanampe <champis_97@hotmail.com.ar>
2021-08-28 20:01:13 -0300

    TP3_Spring[Cambio en codigo de TestClaseAuto para que en un solo test se valide que no se pasen atributos nulos]

--- Prog3_TP3_Spring/src/test/java/testDominio/TestClaseAuto.java
+++ Prog3_TP3_Spring/src/test/java/testDominio/TestClaseAuto.java
@@ -23,63 +23,55 @@ public class TestClaseAuto {
 
     @Order(2)
     @Test
-    public void test02_excepcionAuto_marcaNula(){
+    public void test02_creacionObjetoAuto_excepcionAtributoNulo(){
         assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,null,"147","Blanco",1.1,"XYZ 134",3,1996));
+
+        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat",null,"Blanco",1.1,"XYZ 134",3,1996));
+
+        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat","147",null,1.1,"XYZ 134",3,1996));
+
+        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat","147","Blanco",1.1,null,3,1996));
+
+
     }
 
     @Order(3)
-    @Test
-    public void test03_excepcionAuto_modeloNulo(){
-        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat",null,"Blanco",1.1,"XYZ 134",3,1996));
-    }
-
-    @Order(4)
-    @Test
-    public void test04_excepcionAuto_colorNulo(){
-        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat","147",null,1.1,"XYZ 134",3,1996));
-    }
-
-    @Order(5)
-    @Test
-    public void test05_excepcionAuto_patenteNula(){
-        assertThrows(ExceptionAutoAtributoNulo.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",1.1,null,3,1996));
-    }
-
-    @Order(6)
     @Test
     public void test06_excepcionAuto_marcaVacia(){
         assertThrows(ExceptionAutoAtributoVacia.class, ()-> Auto.factoryAuto(1,"","147","blanco",1.1,"XYZ 123",3,1996));
     }
 
-    @Order(7)
+    @Order(4)
     @Test
     public void test07_excepcionAuto_modeloVacio(){
         assertThrows(ExceptionAutoAtributoVacia.class, ()-> Auto.factoryAuto(1,"Fiat","","blanco",1.1,"XYZ 123",3,1996));
     }
 
-    @Order(8)
+    @Order(5)
     @Test
     public void test08_excepcionAuto_patenteVacia(){
         assertThrows(ExceptionAutoAtributoVacia.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",1.1,"",3,1996));
     }
 
-    @Order(9)
+    @Order(6)
     @Test
     public void test09_excepcionAuto_cilindradaNegativa(){
         assertThrows(ExceptionAutoCilindradaNegativa.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",-1.1,"XTZ 123",3,1996));
     }
 
-    @Order(10)
+    @Order(7)
     @Test
     public void test10_excepcionAuto_cantidadInvalida_de_puertas(){
         assertThrows(ExceptionAutoNumeroPuertasInvalido.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",1.1,"XTZ 123",6,1996));
     }
 
-    @Order(11)
+    @Order(8)
     @Test
     public void test11_excepcionAuto_añoInvalido(){
         assertThrows(ExceptionAutoAñoInvalido.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",1.1,"XTZ 123",5,1613));
 
         assertThrows(ExceptionAutoAñoInvalido.class, ()-> Auto.factoryAuto(1,"Fiat","147","blanco",1.1,"XTZ 123",5,4160));
     }
+
+
 }
