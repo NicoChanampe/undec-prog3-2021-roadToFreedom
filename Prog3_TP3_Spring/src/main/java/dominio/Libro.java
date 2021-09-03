0792906bb2a535f79891e305d53f11638134deec
NicoChanampe <champis_97@hotmail.com.ar>
2021-09-02 22:52:26 -0300

    TP3_Spring[se genero test y codigo para crear un objeto de tipo Libro]

---
+++ Prog3_TP3_Spring/src/main/java/dominio/Libro.java
@@ -0,0 +1,27 @@
+package dominio;
+
+public class Libro {
+    private Long id;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private Integer anioEdicion;
+    private Integer paginas;
+    private Double precio;
+
+    private Libro(Long id, String isbn, String titulo, String autor, String editorial, Integer anioEdicion, Integer paginas, Double precio) {
+        this.id = id;
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.anioEdicion = anioEdicion;
+        this.paginas = paginas;
+        this.precio = precio;
+    }
+
+    public static Libro factoryLibro(Long id, String isbn, String titulo, String autor, String editorial, Integer anioEdicion, Integer paginas, Double precio) {
+        return new Libro(id,isbn,titulo,autor,editorial,anioEdicion,paginas,precio);
+    }
+}
---
+++ Prog3_TP3_Spring/src/test/java/testDominio/TestClaseLibro.java
@@ -0,0 +1,22 @@
+package testDominio;
+
+import dominio.Libro;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
+public class TestClaseLibro {
+    @Order(1)
+    @Test
+    public void test_creacionLibro_datosCorrectosyCompletos_libroCreado(){
+        Libro nuevoLibro = Libro.factoryLibro(1L, "1530-1113-5004-4","Las Crónicas de Narnia: El Principe Caspian", "J. C. Lewis", "LA NACION", 2008,280,650.59);
+        assertNotNull(nuevoLibro);
+    }
+}
